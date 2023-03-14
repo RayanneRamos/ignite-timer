f3f344097b23537ce8051da6d9c01874e05d909a
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-03-14 14:52:40 -0300

    renomeando os arquivos

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
-import { History } from './pages/History/History'
+import { History } from './pages/History'
 import { Home } from './pages/Home'
 
 export function Router() {
